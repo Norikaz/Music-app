787ff791017aa479b265751407de2d8505d6a17d
Norikaz <norikzhagui@gmail.com>
2022-11-14 16:32:07 -0500

    New component for home page

---
+++ client/src/components/Top10List.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+export const Top10List = (props) => {
+    function Card(props){
+        return (
+            <h1>hi </h1>
+        );
+    }
+
+
+
+
+  return (
+      <div className="bg-gray-300 mx-auto my-6 rounded-xl max-w-[95%] w-30">
+        <Card/>
+      </div>
+  );
+};
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,41 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { SearchBar } from "../components/SearchBar";
-
+import { Top10List } from "../components/Top10List";
 //this is the landing page of the website
 //https://www.figma.com/file/0BuMDTJLOjiYCjR997Lrif/muschart?node-id=0%3A1
+
+
+
+
 export const HomePage = (props) => {
+
+    const [playlistInfo, setPlaylistInfo] = useState("")
+        
+    useEffect(() => {
+        if(playlistInfo === "" && props.token){
+        //Grabs Todays Top Hits playlist from spotify  
+        axios(
+            `https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M`,
+            {
+                headers: {
+                    Authorization: "Bearer " + props.token,
+                },
+                method: "GET",
+            }
+        ).then(res => {
+            setPlaylistInfo(res.data.tracks)
+        })
+        .catch(console.log("error"))
+    }
+    },[playlistInfo])
+    console.log(playlistInfo)
     return (
         <div>
             <h1 className="text-4xl font-bold mb-6 my-6 text-center">
                 Find Your Next Favorite Song!
             </h1>
+            <Top10List />
         </div>
     );
 };
