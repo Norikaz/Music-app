095f6d13956d3323d9014b9a1efa61344bc7b581
Norikaz <norikzhagui@gmail.com>
2022-11-15 15:14:30 -0500

    displaying top ten tracks on home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,32 +10,12 @@ import { Top10List } from "../components/Top10List";
 
 export const HomePage = (props) => {
 
-    const [playlistInfo, setPlaylistInfo] = useState("")
-        
-    useEffect(() => {
-        if(playlistInfo === "" && props.token){
-        //Grabs Todays Top Hits playlist from spotify  
-        axios(
-            `https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M`,
-            {
-                headers: {
-                    Authorization: "Bearer " + props.token,
-                },
-                method: "GET",
-            }
-        ).then(res => {
-            setPlaylistInfo(res.data.tracks)
-        })
-        .catch(console.log("error"))
-    }
-    },[playlistInfo])
-    console.log(playlistInfo)
     return (
         <div>
             <h1 className="text-4xl font-bold mb-6 my-6 text-center">
                 Find Your Next Favorite Song!
             </h1>
-            <Top10List />
+            <Top10List token={props.token}/>
         </div>
     );
 };
