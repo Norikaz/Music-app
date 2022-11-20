78f27724dbb8a567d5edf318c500e8f61a7b66dd
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-11-20 12:22:32 -0500

    Styled song info using grid

--- client/src/components/SongInfo.js
+++ client/src/components/SongInfo.js
@@ -3,71 +3,86 @@ import { Card } from "./UI/Card";
 
 //displays the song information on SearchSongPage
 export const SongInfo = (props) => {
+
   return (
-      <div className="bg-gray-100 mx-auto my-6 rounded-xl max-w-[95%] w-30 p-16 font-inter ">
-          <h1 className="font-semibold text-4xl text-left">{props.songName}</h1>
-          <div className="flex flex-col lg:flex-row justify-start">
-              {/* main cover image */}
-              <img
-                  src={props.albumCover}
-                  alt="album cover"
-                  width="411"
-                  height="auto"
-                  className="align-center mt-8 mr-8 rounded-xl"
-              />
+    <div className="bg-gray-100 mx-auto my-6 rounded-xl max-w-[95%] w-30 p-16 font-inter">
+      <h1 className="font-semibold text-4xl text-left">{props.songName}</h1>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 ">
+        {/* main cover image */}
+        <img
+          src={props.albumCover}
+          alt="album cover"
+          width="411"
+          height="auto"
+          className="align-center mt-8 rounded-xl"
+        />
 
-              <div className="inline-flex flex-col xl:flex-row items-center mt-8 w-max ">
-                  {/* displays the artist information */}
-                  <div className="bg-gray-200 outline outline-2 outline-gray-300 inline-flex pr-8 mb-8 mr-8 rounded-xl items-center">
-                      <img
-                          src={props.artistImage}
-                          alt="artist image"
-                          width="125"
-                          className="mr-4 rounded-xl aspect-square"
-                      />
+        <div className="grid 2xl:grid-rows-3 2xl:grid-cols-2 grid-rows-5 grid-cols-1 gap-3 mt-8">
+          {/* displays the song ratings information */}
 
-                      <div className="text-left my-4 text-l">
-                          <span className="font-semibold text-2xl">
-                              {props.artistName[0]}
-                          </span>
-                          {props.artistName.slice(1)}
-                      </div>
-                  </div>
-
-                  {/* displays the album information */}
-                  <div className="bg-gray-200 outline outline-2 outline-gray-300 inline-flex pl-4 mb-8 rounded-xl items-center max-w-md">
-                      <div className="text-right my-4 text-l">
-                          <span className="font-semibold text-2xl">
-                              {props.albumName}
-                          </span>
-                          <p>{props.releaseDate}</p>
-                      </div>
-
-                      <img
-                          src={props.albumCover}
-                          alt="album cover"
-                          width="125"
-                          className="ml-4 rounded-xl aspect-square"
-                      />
-                  </div>
-              </div>
-              <div className="bg-gray-200 outline outline-2 outline-gray-300 rounded-xl p-5 ml-8 inline-flex items-center">
-                  <div className="text-left my-4 text-2xl font-semibold outline-dotted">
-                      Genres:{" "}
-                  </div>
-                  <br />
-                  <div className="my-4 text-l outline-dotted">
-                      {props.genres}
-                  </div>
-              </div>
+          <div className="2xl:col-span-2 col-span-1 grid grid-rows-2 grid-cols-3 grid-flow-col bg-gray-200 outline outline-2 outline-gray-300 rounded-xl items-center">
+            <h2 className="font-semibold text-2xl">Average Score</h2>
+            <p className="text-xl">
+              9.99 <span className="text-2xl">★</span>
+            </p>
+            <h2 className="font-semibold text-2xl">Your Score</h2>
+            <p className="text-xl">
+              9.99 <span className="text-2xl">★</span>
+            </p>
+            <h2 className="font-semibold text-2xl text-center"> Ranked </h2>
+            <p className="text-xl">#{props.popularity}</p>
           </div>
 
+          {/* displays the artist information */}
+          <div className="bg-gray-200 outline outline-2 outline-gray-300 max-h-[7.8rem] inline-flex pr-4 my-4 rounded-xl items-center">
+            <img
+              src={props.artistImage}
+              alt="artist image"
+              width="125"
+              className="mr-4 rounded-xl aspect-square"
+            />
+
+            <div className="text-left my-4 text-l">
+              <span className="font-semibold text-2xl">
+                {props.artistName[0]}
+              </span>
+              {props.artistName.slice(1)}
+            </div>
+          </div>
+
+          {/* displays the album information */}
+          <div className="bg-gray-200 outline outline-2 outline-gray-300 max-h-[7.8rem] inline-flex pl-4 my-4 rounded-xl items-center justify-end">
+            <div className="text-right my-4 text-l">
+              <span className="font-semibold text-2xl">{props.albumName}</span>
+              <p>{props.releaseDate}</p>
+            </div>
+
+            <img
+              src={props.albumCover}
+              alt="album cover"
+              width="125"
+              className="ml-4 rounded-xl aspect-square"
+            />
+          </div>
+
+          {/*displays genre information */}
+          <div className="bg-gray-200 outline outline-2 outline-gray-300 max-h-[7.8rem] inline-flex p-5 rounded-xl items-center">
+            <div className="text-left my-4 text-2xl font-semibold">
+              Genres:{" "}
+            </div>
+            <br />
+            <div className="my-4 ml-4 text-l">{props.genres}</div>
+          </div>
+
+          {/*displays spotify widget */}
           <iframe
-              src={props.id}
-              width="auto"
-              height="152"
-              className="mt-8"
+            src={props.id}
+            height="100%"
+            width="100%"
+            className="inline-flex"
           ></iframe>
+        </div>
       </div>
+    </div>
   );
 };
--- client/src/pages/SongDetailsPage.js
+++ client/src/pages/SongDetailsPage.js
@@ -8,67 +8,69 @@ import useAxiosFetchSpotify from "../components/hooks/useAxiosFetchSpotify";
 //this will be the song page that will display information about each song
 //https://www.figma.com/file/0BuMDTJLOjiYCjR997Lrif/muschart?node-id=34%3A230
 export const SongPage = (props) => {
-    const [artistInfo, setArtistInfo] = useState(null);
+  const [artistInfo, setArtistInfo] = useState(null);
 
-    const token = useContext(TokenContext);
-    const { songInfo, setSongInfo } = useContext(SongInfoContext);
+  const token = useContext(TokenContext);
+  const { songInfo, setSongInfo } = useContext(SongInfoContext);
 
-    //get artist information by artist id provided by songInfo
-    const { isLoading, isError, data } = useAxiosFetchSpotify(
-        () => {
-            if (songInfo)
-                return `https://api.spotify.com/v1/artists/${songInfo.artists[0].id}`;
-        },
-        token,
-        [songInfo]
-    );
+  //get artist information by artist id provided by songInfo
+  const { isLoading, isError, data } = useAxiosFetchSpotify(
+    () => {
+      if (songInfo)
+        return `https://api.spotify.com/v1/artists/${songInfo.artists[0].id}`;
+    },
+    token,
+    [songInfo]
+  );
 
-    console.log("isLoading: " + isLoading);
-    console.log("isError: " + isError);
-    console.log("data: " + data);
+  console.log("isLoading: " + isLoading);
+  console.log("isError: " + isError);
 
-    useEffect(() => {
-        setArtistInfo(data);
-    }, [data]);
+  useEffect(() => {
+    setArtistInfo(data);
+  }, [data]);
 
-    return (
-        <div className="text-center mt-12">
-            <SearchBar />
-            <div>
-                <SongInfo
-                    songName={songInfo ? songInfo.name : "N/A"}
-                    albumName={songInfo ? songInfo.album.name : "N/A"}
-                    releaseDate={songInfo ? songInfo.album.release_date : "N/A"}
-                    albumCover={
-                        songInfo
-                            ? `${songInfo.album.images[1].url}`
-                            : "https://via.placeholder.com/150?text=No+Image"
-                    }
-                    artistName={
-                        songInfo
-                            ? songInfo.artists.map((artist) => (
-                                  <p>{artist.name}</p>
-                              ))
-                            : ["N/A"]
-                    }
-                    artistImage={
-                        artistInfo
-                            ? artistInfo.images[2].url
-                            : "https://via.placeholder.com/150?text=No+Image"
-                    }
-                    genres={
-                        artistInfo
-                            ? artistInfo.genres.map((genre) => <p>{genre}, </p>)
-                            : "N/A"
-                    }
-                    id={
-                        songInfo
-                            ? "https://open.spotify.com/embed/track/" +
-                              songInfo.id
-                            : ""
-                    }
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="text-center mt-12">
+      <SearchBar />
+      <div>
+        <SongInfo
+          songName={songInfo ? songInfo.name : "N/A"}
+          albumName={songInfo ? songInfo.album.name : "N/A"}
+          releaseDate={songInfo ? songInfo.album.release_date : "N/A"}
+          popularity={songInfo ? songInfo.popularity : "N/A"}
+          albumCover={
+            songInfo
+              ? `${songInfo.album.images[1].url}`
+              : "https://via.placeholder.com/150?text=No+Image"
+          }
+          artistName={
+            songInfo
+              ? songInfo.artists.map((artist) => <p>{artist.name}</p>)
+              : ["N/A"]
+          }
+          artistImage={
+            artistInfo
+              ? artistInfo.images[2].url
+              : "https://via.placeholder.com/150?text=No+Image"
+          }
+          genres={
+            artistInfo
+              ? artistInfo.genres.map(
+                  (genre, i) =>
+                    genre.charAt(0).toUpperCase() +
+                    genre.slice(1) +
+                    (i + 1 === artistInfo.genres.length ? "." : ", ")
+                )
+              : "N/A"
+          }
+          id={
+            songInfo
+              ? "https://open.spotify.com/embed/track/" + songInfo.id
+              : ""
+          }
+        />
+      </div>
+    </div>
+  );
 };
